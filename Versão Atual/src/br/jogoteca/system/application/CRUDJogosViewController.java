43b3c5dfea4227d6c684d27131238e0a7bcdae83
Christian <christiandoramo@gmail.com>
2023-03-18 17:18:53 -0300

    menu criaçao

--- Versão Atual/src/br/jogoteca/system/application/CRUDJogosViewController.java
+++ Versão Atual/src/br/jogoteca/system/application/CRUDJogosViewController.java
@@ -8,6 +8,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 public class CRUDJogosViewController implements Initializable {
 	@FXML
@@ -21,19 +25,40 @@ public class CRUDJogosViewController implements Initializable {
 	@FXML
 	protected MenuItem itemAtualizar;
 	
+	@FXML 
+	protected TextField name;
+	@FXML 
+	protected TextField genre;
+	@FXML 
+	protected TextField subGenre;
+	@FXML 
+	protected TextArea description;
+	@FXML 
+	protected TextField price;
+	@FXML 
+	protected TextField releaseDate;
+	@FXML 
+	protected TextField urlImage;
+	@FXML 
+	protected ImageView image;
+	@FXML
+	protected AnchorPane telaInserir;
 	
 	@FXML
 	protected void voltarParaPrincipalAdmin(ActionEvent event) {
 		ViewsController.changeScreen(Tela.PRINCIPALADMIN);
+
 	}
 
 	@FXML
 	protected void mostrarOpcoesCREATE(ActionEvent event) {
 		menuItens.setText("Inserir novo Jogo");
+		telaInserir.setVisible(true);
 	}
 	@FXML
 	protected void mostrarOpcoesREAD(ActionEvent event) {
 		menuItens.setText("Buscar Jogo");
+		telaInserir.setVisible(false);
 	}
 	@FXML
 	protected void mostrarOpcoesUPDATE(ActionEvent event) {
