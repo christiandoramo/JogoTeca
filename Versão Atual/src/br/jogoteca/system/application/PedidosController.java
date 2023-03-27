b07cdbd5888d3a93d52b22cdfb7490185a4e6310
caio10012 <caiofontes183@gmail.com>
2023-03-27 14:50:47 -0300

    viewController

--- Versão Atual/src/br/jogoteca/system/application/PedidosController.java
+++ Versão Atual/src/br/jogoteca/system/application/PedidosController.java
@@ -1,128 +1,71 @@
 package br.jogoteca.system.application;
-
 import java.net.URL;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
-
 import br.jogoteca.system.controllers.PedidoController;
-
 import br.jogoteca.system.exceptions.ElementDoesNotExistException;
-
-import br.jogoteca.system.models.Usuario;
-import javafx.event.ActionEvent;
+import br.jogoteca.system.models.Pedido;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class PedidosController implements Initializable {
 
-	@FXML
-	private ListView<String> elementListView;
 
-	@FXML
-	private TextField nameTextField;
-
-	@FXML
-	private TextField descriptionTextField;
-
-	@FXML
-	private Button addButton;
-
-	@FXML
-	private Button updateButton;
-	
-	@FXML
-	private Button updateButton1;
-
-	@FXML
-	private Button deleteButton;
-	
-	@FXML
-	private DatePicker datePicker;
-
-	
 	 @FXML
-	 private TextField barraDeBusca;
+    private ListView<Pedido> listPedido;
 
-	private List<Usuario> elements = new ArrayList<>();
-	
-	
-	PedidoController pedidos = PedidoController.getInstance();
-	
-//teste
-	//@FXML
-	//void addButton(MouseEvent event) {
-		//String id = nameTextField.getText();
-		//String pedido = descriptionTextField.getText();		
-		//Usuario element = new Usuario(id, pedido);
-		//elements.add(element);
-		//elementListView.getItems().add(element.getPedido());
-		//elementListView.getItems().add(element.getId());		
-		//pedidos.adicionarPedido(pedido,id);+
-	//}
-	@FXML
-	void addButton(MouseEvent event) {
-		String id = nameTextField.getText();
-		String pedido = descriptionTextField.getText();		
-		Usuario element = new Usuario(id, pedido);
-		elements.add(element);
-		elementListView.getItems().add(element.getPedido());
-		elementListView.getItems().add(element.getId());		
-		
-	}
-	 
-		 	           	  
-	
-	@FXML
-	void deleteButton(MouseEvent event) {
-		int selectedIndex = elementListView.getSelectionModel().getSelectedIndex();
-		if (selectedIndex != -1) {	
-			elementListView.getItems().remove(selectedIndex);	
-			elements.remove(selectedIndex);
-		}
-	}
-	
+     @FXML
+     private TextField textId;
 
-	@FXML
-	void updateButton(MouseEvent event) {
-		int selectedIndex = elementListView.getSelectionModel().getSelectedIndex();
-		if (selectedIndex != -1) {			
-			String pedido = descriptionTextField.getText();			
-			elementListView.getItems().set(selectedIndex, pedido);
-			
-			
-		}
-	}
-	@FXML
-	void updateButton1(MouseEvent event) {
-	int selectedIndex = elementListView.getSelectionModel().getSelectedIndex();
-		if (selectedIndex != -1) {
-			String id = nameTextField.getText();								
-			elementListView.getItems().set(selectedIndex,id);
-						
-		}
-	}
-	
-	 @FXML
-	    void buscar(MouseEvent event) throws ElementDoesNotExistException {
-		 String searchText = barraDeBusca.getText().toLowerCase();		 
-		 for (String item : elementListView.getItems()) {
-			    if (item.toLowerCase().contains(searchText)) {
-			    	elementListView.getSelectionModel().select(item);	
-			    					
+   @FXML
+    private TextField textItem;
+    
+    
+    PedidoController ped = PedidoController.getInstance();
+    
+    
+    
+    
+   
+
+    @FXML
+     void buscarId(MouseEvent event) {
+    	int id = Integer.parseInt(textId.getText());
+    	List<Pedido> pedidosAchados = new ArrayList<>();
+    			if (id > 0) {
+			try {
+				Pedido p = ped.buscarPeloId(id);
+				if (p != null) {
+					pedidosAchados.add(p);										
+					System.out.println(p.getMomento());
+					} else {
+					System.out.println("nao tem esse Id");
+					throw new ElementDoesNotExistException(p);	
+					}
+				} catch (Exception e) {
+				 if (e instanceof ElementDoesNotExistException) {
+					 System.out.println("erro");
+			      
+			    	 } else {
+			    	System.out.println("erro");
+			    	   }
 			    }
 			}
-	    }
+		 }
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+     @FXML
+    void buscarItem(MouseEvent event) {
 
+    	 }
+
+
+    @Override
+		public void initialize(URL location, ResourceBundle resources) {
+		 //TODO Auto-generated method stub
+		
 	}
 }
--- Versão Atual/src/br/jogoteca/system/controllers/PedidoController.java
+++ Versão Atual/src/br/jogoteca/system/controllers/PedidoController.java
@@ -1,28 +1,26 @@
 package br.jogoteca.system.controllers;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-
+import java.util.ArrayList;
+import java.util.List;
 import br.jogoteca.system.data.GenericRepository;
 import br.jogoteca.system.data.IGenericRepository;
 import br.jogoteca.system.exceptions.ElementAlreadyExistsException;
 import br.jogoteca.system.exceptions.ElementDoesNotExistException;
 import br.jogoteca.system.exceptions.ElementWithSameNameExistsException;
-import br.jogoteca.system.models.Game;
 import br.jogoteca.system.models.GameItem;
-import br.jogoteca.system.models.Genre;
-
+import br.jogoteca.system.models.Pedido;
 import br.jogoteca.system.models.Usuario;
 
 public class PedidoController {
 	
 	
-	private IGenericRepository<Usuario> pedidoRepositorio;
+	private IGenericRepository<Pedido> pedidoRepositorio;
 	private int lastId = 1;
 	
 	private static PedidoController instance;
 	
-	
+	private ArrayList<Pedido> pedid;
 	
 	private PedidoController() {
 		this.pedidoRepositorio = new GenericRepository<>("Pedidos.dat");
@@ -38,47 +36,61 @@ public class PedidoController {
 	}
 	
 	
-	public void adicionarPedido(String id,String pedido) {
-		Usuario usuario = new Usuario(id,pedido);
+	public void adicionarPedido(LocalDateTime momento, List<GameItem> itens, Usuario usuario) {
+		Pedido pedidos = new Pedido(lastId +1,momento,itens,usuario);
 		try {
-			pedidoRepositorio.insert(usuario);
+			pedidoRepositorio.insert(pedidos);
+			lastId++;			
 		} catch (ElementAlreadyExistsException e) {
 			System.out.println("erro");
 		}
 	}
-	
-	
-	public Usuario buscarPeloId(String id) throws ElementDoesNotExistException{
-			return pedidoRepositorio.read().stream().filter(usuario -> usuario.getId().equals(id)).findFirst().orElse(null);
-		
-	}
-	public Usuario buscarPeloNome(String pedido) throws ElementDoesNotExistException {
-		return pedidoRepositorio.read().stream().filter(usuario -> usuario.getPedido().equals(pedido)).findFirst().orElse(null);
+				
+	public Pedido buscarPeloId(int id) throws ElementDoesNotExistException{
+			return pedidoRepositorio.read().stream().filter(pedido -> pedido.getId() == id).findFirst().orElse(null);		
 	}
 	
-	public void removerPedidoId(String id) throws ElementDoesNotExistException {
-		Usuario usuario = buscarPeloId(id);			
+	public Pedido buscarPeloUsuario(Usuario usuario) throws ElementDoesNotExistException {
+		return pedidoRepositorio.read().stream().filter(pedido -> pedido.getUsuario().equals(usuario)).findFirst().orElse(null);
+	}
+	
+	public Pedido buscarPeloGameItem(List<GameItem> itens) throws ElementDoesNotExistException {
+		return pedidoRepositorio.read().stream().filter(pedido -> pedido.getItens().equals(itens)).findFirst().orElse(null);
+	}		
+	
+	public Pedido buscarPeloMomento(LocalDateTime momento) throws ElementDoesNotExistException {
+		return pedidoRepositorio.read().stream().filter(pedido -> pedido.getMomento().equals(momento)).findFirst().orElse(null);
+	}		
+	
+	public void removerPedidoId(int id) throws ElementDoesNotExistException {
+		Pedido pedido = buscarPeloId(id);			
 			try {
-				pedidoRepositorio.delete(usuario);
+				pedidoRepositorio.delete(pedido);
 				lastId--;
 			} catch (Exception e) {
 				System.out.println("erro");
 			}
 		}
 	
-	public void updatePedidoId(String id, String pedido) throws ElementDoesNotExistException{			
-		Usuario pedido1 = buscarPeloId(id);
-		if (pedido1 != null) {
-			if (id != null && !id.equals(""))
-				pedido1.setId(id);
-			if (pedido != null && !pedido.equals(""))
-				pedido1.setId(pedido);					
-			pedidoRepositorio.update(pedido1);
+	public Pedido buscarId(int id) {
+		for (Pedido pedido : this.pedid) {
+			if (pedido.getId() == id) {
+				return pedido;
 			}
 		}
+		return null;
+	}
 	
-	
-	
-	
-	
-	}	
+	public void updateGameById(int id, LocalDateTime momento, List<GameItem> itens, Usuario usuario)throws ElementDoesNotExistException, ElementWithSameNameExistsException {			 
+		Pedido pedido = buscarPeloId(id);
+		if (pedido != null) {
+			if (itens != null)
+				pedido.setItens(itens);			
+			if (usuario != null)
+				pedido.setUsuario(usuario);
+			if (momento != null)
+				pedido.setMomento(momento);
+			pedidoRepositorio.update(pedido);
+		}
+	}			
+}	
