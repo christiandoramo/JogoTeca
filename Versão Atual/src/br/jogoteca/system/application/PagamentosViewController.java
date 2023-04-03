1a367c5066b7bfa9607020a34c472de9a95bfeb8
Christian <christiandoramo@gmail.com>
2023-04-03 15:52:44 -0300

    tela pagamento

---
+++ Versão Atual/src/br/jogoteca/system/application/PagamentosViewController.java
@@ -0,0 +1,16 @@
+package br.jogoteca.system.application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class PagamentosViewController implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
