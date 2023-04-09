1a86949ea8e18a7eb2b1e171452daa04ac6afeb6
caio10012 <caiofontes183@gmail.com>
2023-04-09 20:50:33 -0300

    testes

--- JogotecaEclipse/src/br/jogoteca/system/application/acessarea/PagamentosViewController.java
+++ JogotecaEclipse/src/br/jogoteca/system/application/acessarea/PagamentosViewController.java
@@ -1,7 +1,9 @@
 package br.jogoteca.system.application.acessarea;
 
 import java.io.IOException;
+
 import java.net.URL;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -10,7 +12,11 @@ import br.jogoteca.system.controllers.PedidoController;
 import br.jogoteca.system.controllers.VendaController;
 import br.jogoteca.system.exceptions.ElementAlreadyExistsException;
 import br.jogoteca.system.exceptions.ElementWithSameNameExistsException;
+import br.jogoteca.system.models.Game;
+import br.jogoteca.system.models.GameItem;
+import br.jogoteca.system.models.Metodo;
 import br.jogoteca.system.models.Pedido;
+import br.jogoteca.system.models.User;
 import br.jogoteca.system.models.Venda;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -18,6 +24,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -56,9 +64,10 @@ public class PagamentosViewController extends AcessAreaController implements Ini
     
     VendaController vc = VendaController.getInstance();
     
-    private Pedido ped;
+   
     private List<String> nova;
     
+  
     
     
     @FXML
@@ -69,6 +78,7 @@ public class PagamentosViewController extends AcessAreaController implements Ini
 
     @FXML
     void butaoPagamento(ActionEvent event) throws ElementAlreadyExistsException, ElementWithSameNameExistsException {
+    	if(preencheuEntradasInsercao()) {
     		Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
     		alert.setTitle("confirmando");
     		alert.setContentText("Pagamento confirmado!");
@@ -79,13 +89,64 @@ public class PagamentosViewController extends AcessAreaController implements Ini
     		nova.add(numero);
     		nova.add(data);
     		nova.add(validade);
-    		vc.insertVenda(ped,nova);
-    		
-    		
-    		
-    		
-    		
+    		vc.insertVenda(pedidoAtual,nova);
+    		pc.adicionarPedido(vencimentoAtual, itemAtual, usuarioAtual, Metodo.DEBITO);  
+    	}
     }
+    
+    @FXML
+    void butaoPagamento2(ActionEvent event) throws ElementAlreadyExistsException, ElementWithSameNameExistsException {
+    	if(preencheuEntradasInsercao2()) {
+    	Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+		alert.setTitle("confirmando");
+		alert.setContentText("Pagamento confirmado!");
+		Optional<ButtonType> result = alert.showAndWait();
+		String numero1 = textNumeroDebito.getText();
+		String data1 = textNumeroDebito.getText();
+		String validade1 = textNumeroDebito.getText();
+		nova.add(numero1);
+		nova.add(data1);
+		nova.add(validade1);
+		vc.insertVenda(pedidoAtual, nova);
+		pc.adicionarPedido(vencimentoAtual, itemAtual, usuarioAtual, Metodo.CREDITO);
+    	}	
+    }
+    
+    @FXML
+    void butaoPagamento3(ActionEvent event) throws ElementAlreadyExistsException, ElementWithSameNameExistsException {
+    	Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+		alert.setTitle("confirmando");
+		alert.setContentText("Pagamento confirmado!");
+		Optional<ButtonType> result = alert.showAndWait();
+    	vc.insertVenda(pedidoAtual,dadoAtual);
+    	pc.adicionarPedido(vencimentoAtual, itemAtual, usuarioAtual, Metodo.PIX);
+    }
+    
+    public boolean preencheuEntradasInsercao() {
+		for (Node node : telaDebito.getChildren()) {
+			if (node instanceof TextField) {
+				TextField tf = (TextField) node;
+				if (tf.getText().trim().isEmpty()) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+    
+    public boolean preencheuEntradasInsercao2() {
+		for (Node node : telaCredito.getChildren()) {
+			if (node instanceof TextField) {
+				TextField tf = (TextField) node;
+				if (tf.getText().trim().isEmpty()) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+    
+    
 
     @FXML
     void butaoVolta(ActionEvent event) throws IOException{
@@ -96,7 +157,7 @@ public class PagamentosViewController extends AcessAreaController implements Ini
     @FXML
     void mostrarOpcoesCredito(ActionEvent event) {   
     	 telaCredito.setVisible(true);
-    	 ped.setMetodo(null);
+    	 
     	 
     	 
     }
