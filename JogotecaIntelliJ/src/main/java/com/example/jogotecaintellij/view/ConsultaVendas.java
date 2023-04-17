ce7994f59c9c20046cc4e8e75867741ca76791eb
caio10012 <caiofontes183@gmail.com>
2023-04-17 10:27:17 -0300

    m

--- JogotecaIntelliJ/src/main/java/com/example/jogotecaintellij/view/ConsultaVendas.java
+++ JogotecaIntelliJ/src/main/java/com/example/jogotecaintellij/view/ConsultaVendas.java
@@ -3,108 +3,82 @@ package com.example.jogotecaintellij.view;
 import com.example.jogotecaintellij.controller.VendaController;
 import com.example.jogotecaintellij.exception.ElementDoesNotExistException;
 import com.example.jogotecaintellij.exception.ElementsDoNotExistException;
+import com.example.jogotecaintellij.model.ItemJogo;
+import com.example.jogotecaintellij.model.Pedido;
 import com.example.jogotecaintellij.model.Venda;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+
 import java.io.IOException;
 import java.net.URL;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class ConsultaVendas extends ViewController implements Initializable {
-    @FXML
-    private TextField textPedido;
-
-    @FXML
-    private TextField textNumero;
-
-    @FXML
-    private ListView<Venda> ListViewVenda;
 
     @FXML
     private Label logBusca;
 
-    VendaController vc = VendaController.getInstance();
+    @FXML
+    private Label logBuscas;
 
     @FXML
-    void buscarCartao(ActionEvent event) {
-        String numero = textNumero.getText();
-        List<Venda> vendasAchados = new ArrayList<>();
-        if (numero != null && !numero.equals("")) {
-            try {
-               Venda n = vc.searchVendaByNumero(textNumero.getText());
-                if (n != null) {
-                    vendasAchados.add(n);
-                    ViewController.mostraVendasAchadas(ListViewVenda, vendasAchados);
-                    logBusca.setVisible(false);
-                    System.out.println(n.getDadosBancarios());
-                } else
-                    System.out.println("número de cartão não encontrado");
-                    throw new ElementDoesNotExistException(n);
-            } catch (Exception e) {
-                logBusca.setText("ERRROO");
-                logBusca.setVisible(true);
-                if (e instanceof ElementDoesNotExistException) {
-                    logBusca.setText("Erro: Venda não encontrada");
-                } else {
-                    logBusca.setText("Ocorreu um erro. Tente novamente.");
-                }
-            }
-        } else {
-            logBusca.setText("Erro: Digite um numero válido");
-            logBusca.setVisible(true);
-        }
-    }
+    private TableView<Pedido> tabelaMinahsVendas;
+
     @FXML
-    void buscarPedido(ActionEvent event) {
-        int id = Integer.parseInt(textPedido.getText());
-        List<Venda> pedidosAchados = new ArrayList<>();
-        if (id > 0) {
-            try {
-                Venda n = vc.searchVendaById(id);
-                if (n != null) {
-                    pedidosAchados.add(n);
-                    ViewController.mostraVendasAchadas( ListViewVenda, pedidosAchados);
-                    logBusca.setVisible(false);
-                    System.out.println(n.getId());
-                } else {
-                    System.out.println("id nao achado");
-                    throw new ElementDoesNotExistException(n);
-                }
-            } catch (Exception e) {
-                if (e instanceof ElementDoesNotExistException) {
-                    logBusca.setText("Erro: Usuario não encontrado");
-                } else {
-                    logBusca.setText("Ocorreu um erro. Tente novamente.");
-                }
-                logBusca.setVisible(true);
-            }
-        } else {
-            logBusca.setText("Erro: Digite um ID válido");
-            logBusca.setVisible(true);
-        }
-    }
+    private TableColumn<Pedido, String> colunaId;
+
     @FXML
-    void searchTodos(ActionEvent event) {
+    private TableColumn<Pedido, String> colunaNomes;
+
+    @FXML
+    private TableColumn<Pedido, String> colunaValor;
+
+    @FXML
+    private TableColumn<Pedido, String> colunaMetodo;
+
+    VendaController vc = VendaController.getInstance();
+
+
+    private void carregaVenda(){
         try {
-            List<Venda> allVendas = vc.searchAllVendas();
-            if (!allVendas.isEmpty()) {
-                ViewController.mostraVendasAchadas( ListViewVenda, allVendas);
-                logBusca.setVisible(false);
-            } else
-                throw new ElementsDoNotExistException(allVendas);
+            ObservableList<Pedido> observableListPedidos = FXCollections.observableList(suc.buscarListaPedidosUsuarioAtual());
+            tabelaMinahsVendas.setItems(observableListPedidos);
+            logBuscas.setVisible(false);
+            colunaId.setCellValueFactory(cellData -> {
+                Pedido pedido = cellData.getValue();
+                return new SimpleStringProperty(Integer.toString(pedido.getId()));
+            });
+            colunaNomes.setCellValueFactory(cellData -> {
+                Pedido pedido = cellData.getValue();
+                List<ItemJogo> itens = pedido.getItens();
+                StringBuilder nomes = new StringBuilder();
+                for (ItemJogo item : itens)
+                    nomes.append(item.getName()).append("\n");
+                return new SimpleStringProperty(nomes.toString());
+            });
+            colunaValor.setCellValueFactory(cellData -> {
+                Pedido pedido = cellData.getValue();
+                return new SimpleStringProperty(String.format("%.2f", pedido.totalValue()));
+            });
+            colunaMetodo.setCellValueFactory(cellData -> {
+                Pedido pedido = cellData.getValue();
+                return new SimpleStringProperty(pedido.getMetodo().name());
+            });
         } catch (Exception e) {
             if (e instanceof ElementsDoNotExistException) {
-                logBusca.setText("Erro: Nenhuma Venda encontrada");
-            } else {
-                logBusca.setText("Ocorreu um erro. Tente novamente.");
-            }
-            logBusca.setVisible(true);
+                logBuscas.setText("Nenhum pedido feito");
+                logBuscas.setVisible(true);
+            } else
+                e.printStackTrace();
         }
     }
     @FXML
@@ -114,6 +88,6 @@ public class ConsultaVendas extends ViewController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        carregaVenda();
     }
 }
